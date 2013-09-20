e279da599e241946a2c7560dad10e7de503fe705
Ivan C. Sanches <ics89@hotmail.com>
2013-09-20 17:41:13 -0400

    More parameters for launch dispatch.

--- src/Water/Library/EventDispatcher/EventDispatcher.php
+++ src/Water/Library/EventDispatcher/EventDispatcher.php
@@ -102,7 +102,7 @@ class EventDispatcher implements EventDispatcherInterface
         }
 
         foreach ($this->getListeners($eventName) as $listener) {
-            call_user_func($listener, $event);
+            call_user_func($listener, $event, $eventName, $this);
 
             if ($event->isStoppedPropagation()) {
                 break;
